c29756889e0c7032aa9209e42b021e2ea4269f29
Peter Czigany <p.czigany@gmail.com>
2024-02-15 12:56:33 +0100

    add start.spring.io base

---
+++ src/main/java/org/peterczigany/userwithelasticsearch/UserWithElasticsearchApplication.java
@@ -0,0 +1,13 @@
+package org.peterczigany.userwithelasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserWithElasticsearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserWithElasticsearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/peterczigany/userwithelasticsearch/UserWithElasticsearchApplicationTests.java
@@ -0,0 +1,13 @@
+package org.peterczigany.userwithelasticsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserWithElasticsearchApplicationTests {
+
+  /** This test is empty on purpose. */
+  @SuppressWarnings("java:S2699")
+  @Test
+  void contextLoads() {}
+}
