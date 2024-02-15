adda8c8b4907c2071148509eb688f9e209b04a01
Peter Czigany <p.czigany@gmail.com>
2024-02-15 14:20:23 +0100

    add acceptance tests for search endpoint

--- src/test/java/org/peterczigany/userwithelasticsearch/SearchForUserAcceptanceTest.java
+++ src/test/java/org/peterczigany/userwithelasticsearch/SearchForUserAcceptanceTest.java
@@ -1,9 +1,16 @@
 package org.peterczigany.userwithelasticsearch;
 
-import org.junit.jupiter.api.Test;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
@@ -16,14 +23,38 @@ class SearchForUserAcceptanceTest {
    * '/api/elasticsearch/search', the response is a UserSearchResponse (total: long [number of
    * matches], userList: List<UserResponse> [user objects matching the search criteria]).
    */
-  @Test
-  void givenRequestIsCorrect_whenRequestIsSent_thenRespondWithUserSearchResponse() {}
+  @ParameterizedTest
+  @CsvFileSource(resources = "/validUserSearchRequestJson.csv")
+  void givenRequestIsCorrect_whenRequestIsSent_thenRespondWithUserSearchResponse(
+      String userSearchRequestJson) throws Exception {
+    mockMvc
+        .perform(
+            post("http://localhost:8080/api/elasticsearch/search")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(userSearchRequestJson))
+        .andExpect(status().isOk())
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+        .andExpect(jsonPath("$.id").isNotEmpty())
+        .andExpect(jsonPath("$.firstName").value("Janos"))
+        .andExpect(jsonPath("$.lastName").value("Szabo"))
+        .andExpect(jsonPath("$.email").value("janos.szabo@szabojanosok.hu"));
+  }
 
   /**
    * If the UserSearchRequest (firstName: string, lastName: string) is not correct, i.e. firstName
    * or lastName is missing, the response should be 400 Bad Request with a message specifying the
    * missing field(s).
    */
-  @Test
-  void givenRequestIsInCorrect_whenRequestIsSent_thenRespondWith400AndMessage() {}
+  @ParameterizedTest
+  @CsvFileSource(resources = "/invalidUserSearchRequestJson.csv")
+  void givenRequestIsInCorrect_whenRequestIsSent_thenRespondWith400AndMessage(
+      String userSearchRequestJson) throws Exception {
+    mockMvc
+        .perform(
+            post("http://localhost:8080/api/elasticsearch/search")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(userSearchRequestJson))
+        .andExpect(status().isBadRequest());
+    // todo: expect error message
+  }
 }
